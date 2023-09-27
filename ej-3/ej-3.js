7738de6818c89b4b2febe60698ab9f94080d6e4e
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-09-27 12:08:04 +0200

    exercise 3

--- ej-3/ej-3.js
+++ ej-3/ej-3.js
@@ -1,2 +1,15 @@
-prompt("Dígame su nombre")
-prompt("¿Qué hora es?")
+function getGreeting() {
+    let name = document.getElementById("name").value;
+    let hour = document.getElementById("hour").value;
+    let greeting;
+
+    if (hour >= 6 && hour < 12) {
+        greeting = "¡Good morning, " + name + "!";
+    } else if (hour >= 12 && hour < 20) {
+        greeting = "¡Good evening, " + name + "!";
+    } else {
+        greeting = "¡Good night, " + name + "!";
+    }
+
+    alert(greeting);
+  }
