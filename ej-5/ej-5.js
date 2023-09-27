b66225ee250bf4837204864cf5c048e5ebc4aba9
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-09-27 12:57:19 +0200

    exercise 5 1º and 3º part

---
+++ ej-5/ej-5.js
@@ -0,0 +1,37 @@
+var valores = [true, 5, false, "hola", "adios", 2]
+
+// Primera parte
+
+// Filtramos solamente los strings
+var obtenerTextos = valores.filter(item => typeof item === 'string')
+
+// Ordenamos los textos por la longitud de los caracteres (de mayor a menor)
+obtenerTextos.sort((a, b) => b.length - a.length)
+
+// Obtenemos ahora el texto más largo del array
+var textoMasLargo = obtenerTextos[0]
+
+alert(textoMasLargo)
+
+// Tercera parte
+
+// Filtramos solamente los números
+var obtenerNúmeros = valores.filter(item => typeof item === 'number')
+
+// Separamos los 2 números en 2 variables
+var num1 = obtenerNúmeros[0]
+var num2 = obtenerNúmeros[1]
+
+var suma = num1 + num2
+var resta = num1 - num2
+var multiplicacion = num1 * num2
+var division = num1 / num2
+var resto = num1 % num2
+
+alert(suma)
+alert(resta)
+alert(multiplicacion)
+alert(division)
+alert(resto)
+
+
