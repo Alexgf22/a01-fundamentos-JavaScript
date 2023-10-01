288ed0d022ee20b39e7e298f7c4c96e7b437fda0
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-10-01 17:44:26 +0200

    Changing syntax

--- ej-3/ej-3-2.js
+++ ej-3/ej-3-2.js
@@ -1,15 +1,15 @@
 function getGreeting() {
-    let name = document.getElementById("name").value;
-    let hour = new Date().getHours();
-    let greeting;
+    let name = document.getElementById("name").value
+    let hour = new Date().getHours()
+    let greeting
 
     if (hour >= 6 && hour < 12) {
-        greeting = "¡Good morning, " + name + "!";
+        greeting = "¡Good morning, " + name + "!"
     } else if (hour >= 12 && hour < 20) {
-        greeting = "¡Good evening, " + name + "!";
+        greeting = "¡Good evening, " + name + "!"
     } else {
-        greeting = "¡Good night, " + name + "!";
+        greeting = "¡Good night, " + name + "!"
     }
 
-    alert(greeting);
+    alert(greeting)
   }
--- ej-8/ej-8.js
+++ ej-8/ej-8.js
@@ -1,14 +1,14 @@
 function isPalindrome(text) {
-    var processedText = text.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
-    var reversedText = processedText.split('').reverse().join('');
+    var processedText = text.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'')
+    var reversedText = processedText.split('').reverse().join('')
     
-    return processedText === reversedText;
+    return processedText === reversedText
 }
 
-var text = prompt("Enter a phrase to see if it is a palindrome");
+var text = prompt("Enter a phrase to see if it is a palindrome")
 
 if (isPalindrome(text)) {
-    alert("The string is a palindrome.");
+    alert("The string is a palindrome.")
 } else {
-    alert("The string is not a palindrome.");
+    alert("The string is not a palindrome.")
 }
