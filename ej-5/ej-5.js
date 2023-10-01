eee4715c0282c9b4c12e666871787a02c74d7932
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-10-01 18:15:39 +0200

    Finishing exercise 5

--- ej-5/ej-5.js
+++ ej-5/ej-5.js
@@ -1,41 +1,45 @@
-var valores = [true, 5, false, "hola", "adios", 2]
+let valores = [true, 5, false, "hola", "adios", 2]
 
 // Primera parte
 
 // Filtramos solamente los strings
-var obtenerTextos = valores.filter(item => typeof item === 'string')
+let obtenerTextos = valores.filter(item => typeof item === 'string')
 
 // Ordenamos los textos por la longitud de los caracteres (de mayor a menor)
 obtenerTextos.sort((a, b) => b.length - a.length)
 
 // Obtenemos ahora el texto más largo del array
-var textoMasLargo = obtenerTextos[0]
+let textoMasLargo = obtenerTextos[0]
 
 alert(textoMasLargo)
 
 // Segunda parte
 
-if (valores[0] == true || valores[0] == false) {
-    alert("Comparando valores booleanos en posición 0 \n del array con '||' es true \n ya que se encuentra en dicho índice uno u otro")
-}
-alert("Comparando valores booleanos en posición 0 \n del array con '&&' es false \n ya que en dicho índice no se pueden encontrar ambos booleanos al mismo tiempo")
+// Sacamos el resultado true
+let resultadoEsTrue = valores[0] || valores[2]
+
+// Sacamos el resultado false
+let resultadoEsFalse = valores[2] && valores[2]
+
+alert("EL resultado es true:" + resultadoEsTrue)
+alert("Resultado es false:" + resultadoEsFalse)
 
 
 
 // Tercera parte
 
 // Filtramos solamente los números
-var obtenerNúmeros = valores.filter(item => typeof item === 'number')
+let obtenerNúmeros = valores.filter(item => typeof item === 'number')
 
 // Separamos los 2 números en 2 variables
-var num1 = obtenerNúmeros[0]
-var num2 = obtenerNúmeros[1]
+let num1 = obtenerNúmeros[0]
+let num2 = obtenerNúmeros[1]
 
-var suma = num1 + num2
-var resta = num1 - num2
-var multiplicacion = num1 * num2
-var division = num1 / num2
-var resto = num1 % num2
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+let division = num1 / num2
+let resto = num1 % num2
 
 alert(num1 + " + " + num2 + " = " + suma)
 alert(num1 + " - " + num2 + " = " + resta)
